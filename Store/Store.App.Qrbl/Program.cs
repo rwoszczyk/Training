5e88d3095355fb22d9000898ea00fd434fba3021
WK-PC\general <rwoszczyk@vp.pl>
2018-06-12 23:46:35 +0200

    queryable

---
+++ Store/Store.App.Qrbl/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Store.Queryable;
+
+namespace Store.App.Qrbl
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      using (var db = new StoreDbContext())
+      {
+        Console.WriteLine("Orders");
+
+
+        var ordersOfToday = db
+          .Orders
+          .ToList()
+          .Where(x => x.SubmitDate == new DateTime(2018, 06, 15));
+
+        PrintOrders(ordersOfToday);
+
+
+        var ordersOfToday_2 = db
+          .Orders
+          .Where(x => x.SubmitDate == new DateTime(2018, 06, 15))
+          .ToList();
+
+        PrintOrders(ordersOfToday_2);
+      }
+
+      Console.ReadLine();
+    }
+
+    private static void PrintOrders(IEnumerable<Order> orders)
+    {
+      foreach (var order in orders)
+      {
+        Console.WriteLine(@"Id: {0}, submit date: {1}", order.Id, order.SubmitDate);
+      }
+    }
+  }
+}
---
+++ Store/Store/Queryable/StoreDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace Store.Queryable
+{
+  public class StoreDbContext : DbContext
+  {
+    public StoreDbContext() : base("Store")
+    {
+#if DEBUG
+      this.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
+#endif
+    }
+
+    public DbSet<Customer> Customers { get; set; }
+
+    public DbSet<Order> Orders { get; set; }
+  }
+}
