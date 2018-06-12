c568d82eadc0db2003ff718666590780fbf831f4
WK-PC\general <rwoszczyk@vp.pl>
2018-06-12 07:41:38 +0200

    init

---
+++ Store/Store.App/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.App
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Store.App");
+
+      using (var db = new StoreDbContext())
+      {
+        var orders = db.Orders.Select(x => x.SubmitDate);
+
+        foreach (var order in orders)
+        {
+          Console.WriteLine("submit date: " + order.SubmitDate);
+        }
+        
+            db.Entry(orders.First());
+
+      }
+
+
+
+      Console.ReadLine();
+    }
+  }
+}
---
+++ Store/Store/StoreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using System.Linq;
+
+namespace Store
+{
+  public partial class StoreDbContext : DbContext
+  {
+    public StoreDbContext() : base("Store")
+    {
+    }
+
+    public DbSet<Customer> Customers { get; set; }
+
+    public DbSet<Order> Orders { get; set; }
+  }
+}
